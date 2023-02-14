8b6196e49325ad618855e42311cb58760f136bf2
USHAPRIYADHARSINI <110903183+USHAPRIYADHARSINI@users.noreply.github.com>
2023-02-14 22:41:56 +0530

    forgot password client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from 'react-router-dom';
+import Signup from './Components/Signup.js';
+import Login from './Components/Login.js';
+import Home from './Components/Home.js';
+import ForgotPassword from './Components/ForgotPassword.js';
+import PasswordReset from './Components/PasswordReset.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route
+          path="/PasswordReset/:email/:token"
+          element={<PasswordReset />}
+        />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Signup.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+function Signup() {
+  let navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  let addUser = () => {
+    const newUser = {
+      name: name,
+      email: email,
+      password: password,
+    };
+    fetch(`http://localhost:5000/users/signup`, {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((data) => data.json())
+      .then(() => {
+        if (name.length < 3 && email.length < 6 && password.length < 6) {
+          alert("Please fill the required field");
+        } else {
+          navigate("/login");
+        }
+      });
+  };
+
+  return (
+    <>
+      <h1>{process.env.REACT_APP_LOCAL_URL}</h1>
+      <h1 style={{ textAlign: "center", fontSize: "30px", padding: "20px" }}>
+        Create your Account
+      </h1>
+      <Box
+        sx={{
+          minWidth: 350,
+          maxWidth: 900,
+          margin: "20px auto",
+          display: "flex",
+          flexDirection: "column",
+          gap: 4,
+          padding: { xs: "30px", md: "20px" },
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          type="text"
+          label="name"
+          onChange={(e) => setName(e.target.value)}
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          type="text"
+          label="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          type="password"
+          label="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          variant="outlined"
+        />
+        <Button sx={{ padding: "15px" }} variant="contained" onClick={addUser}>
+          Submit
+        </Button>
+        <p style={{ textAlign: "center" }}>
+          Already have an account{" "}
+          <span
+            style={{ cursor: "pointer", color: "blue" }}
+            onClick={() => navigate("/login")}
+          >
+            Click here to login
+          </span>
+        </p>
+      </Box>
+    </>
+  );
+}
+
+export default Signup;
